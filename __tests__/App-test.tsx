15ff47c0b91318381a564b45199e6e3b9bd63be9
Cat Lam <tuongcat@cs.washington.edu>
2023-02-07 14:11:57 -0800

    Install react testing-library

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -4,11 +4,17 @@
 
 import 'react-native';
 import React from 'react';
+import {render, fireEvent} from '@testing-library/react-native';
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('<CategoryPicker />', () => {
+  it('', () => {
+    renderer.create(<App />);
+  });
 });
+
+
+
