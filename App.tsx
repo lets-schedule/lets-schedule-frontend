96c52b8efef91c918b5559dfeb87c8215a2eb4e5
j v <vantjac@gmail.com>
2023-02-07 06:02:55 -0800

    Testing EditEventPage

--- App.tsx
+++ App.tsx
@@ -1,59 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState, useCallback} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
-  StyleSheet,
-  Text,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import EditEventPage from './src/components/EditEventPage';
+import {commStyles} from './src/components/Util';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -62,57 +18,39 @@ function App(): JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const [event, setEvent] = useState({id: 0, taskId: 0, startTime: new Date(), endTime: new Date()});
+  const [task, setTask] = useState({title: 'cool event', category: 0, priority: 3});
+  const [repeat, setRepeat] = useState({repeat: true, until: new Date(), days: [true, false, false, true, true, true, false]});
+
+  const handleEventChange = useCallback((e: any) => {
+    setEvent((ePrev) => {return {...ePrev, ...e}});
+    // console.log(e);
+  }, []);
+  const handleTaskChange = useCallback((t: any) => {
+    setTask((tPrev) => {return {...tPrev, ...t}});
+    // console.log(t);
+  }, []);
+  const handleRepeatChange = useCallback((r: any) => {
+    setRepeat((rPrev) => {return {...rPrev, ...r}});
+    // console.log(r);
+  }, []);
+
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={[backgroundStyle, {flex: 1}]}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+      <View style={commStyles.expand}>
+        <EditEventPage
+          event={event} onEventChange={handleEventChange}
+          task={task} onTaskChange={handleTaskChange}
+          repeat={repeat} onRepeatChange={handleRepeatChange} 
+          style={commStyles.grow}
+            />
+      </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
