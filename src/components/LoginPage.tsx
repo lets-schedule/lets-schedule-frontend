2a27d6ca1032d5290477d9a568ea3b5c2429e9c1
j v <vantjac@gmail.com>
2023-03-07 21:31:02 -0800

    show password box when focused

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo, useState } from 'react';
 import { FloatingButton, TextField, View, Text } from 'react-native-ui-lib';
-import { Button, Alert } from 'react-native'
+import { Button, Alert, ScrollView } from 'react-native'
 import { commStyles } from '../Util';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { useIsFocused } from '@react-navigation/native';
@@ -22,16 +22,19 @@ export default function LoginPage({ navigation, onSignInButtonPress, ...props }:
     if (!useIsFocused())
         return <></>
     return (
-        <View style={commStyles.formPage}>
-            <View style={commStyles.expand} />
-            <MaterialCommunityIcons style={{alignSelf: 'center'}} name='account-circle' size={100} />
-            <View style={commStyles.signIn} />
-            <Button title="Create Account" onPress={() => navigation.navigate("SignUp")} />
-            <View style={commStyles.padded} />
-            <TextField placeholder='email' onChangeText={setEmail} />
-            <TextField secureTextEntry placeholder='password' onChangeText={setPassword}/>
-            <View style={commStyles.expand} />
-            <FloatingButton visible={true} button={log_in_button} />
-        </View>
+        <ScrollView showsVerticalScrollIndicator={false}
+                style={{flex: 1}} contentContainerStyle={{flexGrow: 1}}>
+            <View style={commStyles.formPage}>
+                <View style={commStyles.expand} />
+                <MaterialCommunityIcons style={{alignSelf: 'center'}} name='account-circle' size={100} />
+                <View style={commStyles.signIn} />
+                <Button title="Create Account" onPress={() => navigation.navigate("SignUp")} />
+                <View style={commStyles.padded} />
+                <TextField placeholder='email' onChangeText={setEmail} />
+                <TextField secureTextEntry placeholder='password' onChangeText={setPassword}/>
+                <View style={commStyles.expand} />
+                <FloatingButton visible={true} button={log_in_button} />
+            </View>
+        </ScrollView>
     )
 }
