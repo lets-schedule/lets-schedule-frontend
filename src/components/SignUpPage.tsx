457967a2445b1bc223f782d4bcf07eb5bad6a495
j v <vantjac@gmail.com>
2023-03-07 20:59:57 -0800

    Fix sign up

--- src/components/SignUpPage.tsx
+++ src/components/SignUpPage.tsx
@@ -7,32 +7,17 @@ import { useIsFocused } from '@react-navigation/native';
 
 export default function SignUpPage({ navigation, onSignUpButtonPress, ...props }: any) {
 
-    let user_email;
-    let password;
-
-    const handleCaptureEmail = (text:string) => {
-        user_email = text;
-        //console.log(user_email);
-    };
-
-    const handleCapturePassword = (text:string) => {
-        password = text;
-    };
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     const sign_up_button = useMemo(() => {
         return {
             label: 'Create Account',
             onPress: () => {
-                onSignUpButtonPress(user_email, password, navigation);
-                navigation.navigate("MainTabs");
+                onSignUpButtonPress(email, password, navigation);
             }
         }
-    }, [onSignUpButtonPress, navigation]);
-
-    const handleChange = (event) => {
-        console.log(event.target.value);
-        onEmailChange(event.target.value);
-    };
+    }, [onSignUpButtonPress, navigation, email, password]);
 
     if (!useIsFocused())
         return <></>
@@ -41,8 +26,8 @@ export default function SignUpPage({ navigation, onSignUpButtonPress, ...props }
             <View style={commStyles.expand} />
             <MaterialCommunityIcons style={{alignSelf: 'center'}} name='account-circle' size={100} />
             <View style={commStyles.padded} />
-            <TextField id="signup_email" placeholder='email' onChangeText={handleCaptureEmail} />
-            <TextField id="signup_password" secureTextEntry placeholder='password' onChangeText={handleCapturePassword} />
+            <TextField id="signup_email" placeholder='email' onChangeText={setEmail} />
+            <TextField id="signup_password" secureTextEntry placeholder='password' onChangeText={setPassword} />
             <View style={commStyles.expand} />
             <FloatingButton visible={true} button={sign_up_button}/>
         </View>
